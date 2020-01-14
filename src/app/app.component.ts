0e0797e0a64fee92f05a1a435ca4b29330b2d600
Tomas Mali <tmali@sanmarcoinformatica.it>
2020-01-14 12:43:56 +0100

    fixed all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { saveAs } from 'file-saver';
-
-import { Inject }  from '@angular/core';
-import { DOCUMENT } from '@angular/common'; 
-
-
-
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,53 +12,41 @@ export class AppComponent {
   title = 'angularUpload';
   hint = "Choose file"
   fileUploadedContent = ""
+  file: File = null
 
-file: File = null
-
-uploadUrlPath = 'src/assets/uploads/'
+  uploadUrlPath = 'src/assets/uploads/'
   fileReader: any;
   fileString: any;
 
-
-
   constructor(@Inject(DOCUMENT) document) {
-  
- }
-
-
-  onFileSelected(event){
-    console.log(event)
-
-    this.hint = event.target.files[0].name
-
-    if((this.hint).split('.').pop() === "pdf444"){
-      this.hint = null
-      alert("Estensione file non permesso! Selezionare un file compatibile")
-
-     // document.getElementById("openModalButton").click();
-      
- }else
- {
-    // The file content itself
-    this.file =<File> event.target.files[0];
-    this.onText()
- }
   }
 
+  onFileSelected(event) {
+    //  console.log(event)
+    this.hint = event.target.files[0].name
+    if ((this.hint).split('.').pop() === "pdf") {
+      this.hint = null
+      alert("Estensione file non permesso! Selezionare un file compatibile")
+      // document.getElementById("openModalButton").click(); 
+    } else {
+      // The file content itself
+      this.file = <File>event.target.files[0];
+      this.onText()
+    }
+  }
 
-
-  onText(){
+  onText() {
     let fileReader = new FileReader();
     fileReader.onload = (e) => {
- 
-     // Entire file
-      var fileAsString = ( <String> (fileReader.result)).split('\n')
+
+      // Entire file
+      var fileAsString = (<String>(fileReader.result)).split('\n')
       var outputFile = ""
       // Read line by line and construct file
-      for(var line = 0; line < fileAsString.length; line++){
-       console.log(line  + '\n');
-       //Adding whatever you wish   
-       outputFile = outputFile + fileAsString[line] + '\n'
+      for (var line = 0; line < fileAsString.length; line++) {
+        //    console.log(line + '\n');
+        //Adding whatever you wish   
+        outputFile = outputFile + fileAsString[line] + '\n'
       }
       document.getElementById("p1").innerHTML = outputFile
     }
@@ -70,37 +54,140 @@ uploadUrlPath = 'src/assets/uploads/'
   }
 
 
+  // Construct the final file 
+  onUpload() {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
 
-  onUpload(){
+      // Entire file as string[]
+      var fileAsArrayString = (<string>(fileReader.result)).split('\n')
+      var outputFile = ""
+      var iii = -1
 
-    if(this.file === null){
-      alert("Select a file!")
+      // Read line by line and construct file
+      for (var line = 0; line < fileAsArrayString.length; line++) {
 
+        var finalLine = ""
+        var currentLine = fileAsArrayString[line]
+        //###########################################################################################################
+        let split: Array<string> = new Array();
+        split = null
+        split = currentLine.toString().split("\t");
+        iii = iii + 1;
+        // preparo il file di output
+        // salto la prima riga
+        //   console.log("Prima dello switch i vale: " + iii)
+        switch (iii) {
+          case 0:
+            {
+              // idoc: non devo fare nulla
+              outputFile = outputFile + currentLine
+            }
+            continue;
+          case 1:
+            {
+              // record A
+              // controllo se ho garanti    break;
+              var nGaranti = parseInt((split[80]).toString().trim())
+              console.log("\n Iterazione[" + line + "] nGaranti vale :  " + nGaranti + "\n")
+              // se ho iterazioni garanti, per trovare l'iterazione successiva devo moltiplicare questo numero
+              // per
+              // i campi da saltare
+              //   var posDau = 81;
+              var posGaranti = 80
+              // trovo iterazione successiva (Dau)
+              var posDau = this.findNextIteration(parseInt(split[posGaranti].trim()), 10, posGaranti);
+              // trovo iterazione successiva (Trasporto)
+              var posTrasporto = this.findNextIteration(parseInt(split[posDau].trim()), 3, posDau);
+              // trovo iterazione successiva (Certificati)
+              var posCert = this.findNextIteration(parseInt(split[posTrasporto].trim()), 9, posTrasporto);
+              // controllo iterazioni Certificati
+              var nCert = parseInt((split[posCert]).toString().trim());
+              // intanto mi salvo i dati fino a qui.
+              for (var j1 = 0; j1 <= posCert; j1++) {
+                //  console.log("######  Sono in FOR: " + iii + " e split[j1] vale PRIMA DEL METODO: \n" + currentLine+ "\n")
+                finalLine = finalLine + (split[j1] + "\t");
+                //    console.log("######  Sono in FOR: " + iii + " e split[j1] vale DOPO IL METODO: \n" + currentLine+ "\n")
+              }
+              var posRecC = posCert + 1;
+              // se ho certificati devo andare ad aggiornare il tracciato
+              if (nCert > 0) {
+                // trovo la fine dei certificati
+                posRecC = posRecC + (nCert * 6);
+                // per ogni record di certificato scrivo i primi campi as is
+                var countField = 0;
+                for (var j2 = posCert + 1; j2 < posRecC; j2++) {
+                  countField++;
+                  finalLine = finalLine + (split[j2] + "\t");
+                  if (countField == 6) {
+                    countField = 0;
+                    finalLine = finalLine + ("\t" + "\t");
+                  }
+                }
+              }
+              finalLine = finalLine + this.elabRecordC(posRecC, split)
+              outputFile = outputFile + finalLine
+            }
+            break;
+
+          default:
+            {
+              // code block
+              if (currentLine.toString().trim() !== "") {
+                finalLine = finalLine + (split[0] + "\t" + split[1] + "\t" + this.elabRecordC(2, split))
+                outputFile = outputFile + finalLine
+              }
+            }
+            break;
+        }
+        //###########################################################################################################
+      }
+      // Sending final file...[can be fixed some more]
+      saveAs(new Blob([outputFile], { type: 'text/csv;charset=utf-8' }), this.hint + "_ELAB");
+    }
+    fileReader.readAsText(this.file)
+  }
+
+  elabRecordC(posRecC_N, split_N) {
+    var posImba = posRecC_N + 22;
+    var finalC = ""
+    // scrivo la prima parte fissa
+    for (var i1 = posRecC_N; i1 <= posImba; i1++) {
+      finalC = finalC + (split_N[i1] + "\t");
+    }
+    // leggo quanti imballaggi ho
+    var nImba = parseInt(split_N[posImba].toString().trim());
+    var posVino = posImba + 1;
+    // se ho imballaggi devo andare ad aggiornare il tracciato
+    if (nImba > 0) {
+      // trovo la fine dei certificati
+      posVino = posVino + (nImba * 7);
+      // per ogni record di imballaggi scrivo i primi campi as is
+      var countField = 0;
+      for (var j4 = posImba + 1; j4 < posVino; j4++) {
+        countField++;
+        finalC = finalC + (split_N[j4] + "\t");
+        if (countField == 7) {
+          countField = 0;
+          finalC = finalC + ("\t");
+        }
+      }
     }
 
-  
-    console.log("***********   " + document.getElementById('customFile'))
+    // fatto ciò, posso scrivere fino alla fine
+    for (var i5 = posVino; i5 < split_N.length; i5++) {
+      finalC = finalC + (split_N[i5] + "\t");
+    }
 
-   let fileReader = new FileReader();
-   fileReader.onload = (e) => {
-
-    // Entire file
-     var fileAsString = ( <String> (fileReader.result)).split('\n')
-
-
-     var outputFile = ""
-     // Read line by line and construct file
-     for(var line = 0; line < fileAsString.length; line++){
-      console.log(fileAsString[line] + "  @@# ");
-      //Adding whatever you wish
-      outputFile = outputFile + fileAsString[line] + "  @@# "
-     }
-     
-     // Sending final file...[can be fixed some more]
-      saveAs(new Blob([outputFile], { type: 'text/csv;charset=utf-8' }), "fixed.C80");
-   }
-   fileReader.readAsText(this.file)
-
+    finalC = finalC.substring(0, finalC.length - 1);
+    return finalC
+  }
+  findNextIteration(nIter, fieldIter, posIter) {
+    if (nIter > 0)
+      return posIter + 1 + (nIter * fieldIter);
+    return posIter + 1;
   }
 
 }
+
+
