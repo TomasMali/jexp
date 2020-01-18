1e52519b5092b451cbb94053000c6d86321d3843
Tomas <tomas@iMac-di-Tomas.lan>
2020-01-19 00:49:43 +0100

    added style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/
 
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
@@ -16,7 +19,10 @@ import { HttpClientModule } from '@angular/common/http';
 // 2.
 const routes: Routes = [
   // basic routes
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 
@@ -25,7 +31,10 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
